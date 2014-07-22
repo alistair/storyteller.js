7d9162401958e65e92f7f9da61f10027c8a611a7
jeremydmiller <jeremydmiller@yahoo.com>
2014-07-22 09:16:59 -0500

    added mocha and a mocha task to gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var gutil = require('gulp-util');
 
 gulp.task('default', function() {
   console.log('Hello!');
+});
+
+gulp.task('mocha', function(){
+    return gulp.src(['test/test-*.js'], { read: false })
+        .pipe(mocha({
+            reporter: 'spec',
+            globals: {}
+        }));
+});
+
+gulp.task('watch-mocha', function() {
+    gulp.watch(['lib/**', 'test/**'], ['mocha']);
 });
